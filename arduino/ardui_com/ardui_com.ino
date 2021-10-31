c1571a6b4ea7bcfe7e196b5098a54eae18688e5b
Fabien Portas <fabien.portas@grenoble-inp.org>
2021-10-31 12:14:22 -0400

    Add slavce implementation of protocol.

--- arduino/ardui_com/ardui_com.ino
+++ arduino/ardui_com/ardui_com.ino
@@ -1,49 +1,114 @@
 #include <Wire.h>
 #include <stdint.h>
 
-enum RecvState { WAITING_COMMAND, WAITING_COMMAND_DATA };
-enum RecvState state;
-uint8_t valve;
+#include "protocol.hpp"
 
 #define I2C_PORT 0x20
-#define VALID_PORTS_START 38
-#define VALID_PORTS_END 53
+constexpr uint8_t VALID_ACTUATOR_ID_MIN = 38;
+constexpr uint8_t VALID_ACTUATOR_ID_MAX = 53;
+
+enum Error {
+  ERR_NONE = 0, ///< Correct return state.
+  ERR_UNKNOWN ///< Unknown error.
+};
+
+typedef Error (*packet_handler_t)(const BrouePacket*);
+
+static packet_handler_t packet_handlers[BROUE_TYPE_COUNT] = { nullptr };
+static SlaveProtocolState s_state;
+static BrouePacket response_packet = { 0 };
+
+Error actuate_handler(const BrouePacket* pkt) {
+  const BroueActuatePayload& payload = pkt->payload.actuate_payload;
+
+  if (payload.actuator_id >= VALID_ACTUATOR_ID_MIN 
+      && payload.actuator_id <= VALID_ACTUATOR_ID_MAX) {
+    // True => >0 => ouvrir la valve
+    // Sanity check on the pin value
+    digitalWrite(payload.actuator_id, payload.actuator_command ? HIGH : LOW);
+    return ERR_NONE;
+  }
+  return ERR_UNKNOWN;
+}
 
 void setup() {
-  for (int i = VALID_PORTS_START; i <= VALID_PORTS_END; i++) {
+  for (int i = VALID_ACTUATOR_ID_MIN; i <= VALID_ACTUATOR_ID_MAX; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
   }
+
   Wire.begin(I2C_PORT);  // join i2c bus (address optional for master)
   Serial.begin(9600);    // start serial for output
-  // Setup the state
-  state = WAITING_COMMAND;
-  Wire.onReceive(onReceiveCommand);
+
+  Wire.onReceive(&onReceivePacket);
+  Wire.onRequest(&onReadRequest);
 }
 
 void loop() {}
 
-void onReceiveCommand() {
-  // Read the octet on the wire
-  uint8_t octet = Wire.read();
+void onReceivePacket(int num_bytes) {
+  if (num_bytes != BROUE_PACKET_SIZE) {
+    Serial.print("Unexpected packet size on channel, got ");
+    Serial.print(num_bytes);
+    Serial.print(" bytes, expected ");
+    Serial.println(BROUE_PACKET_SIZE);
 
-  // State mahine, initial state => WATING_COMMAND
-  switch (state) {
-    case WAITING_COMMAND:
-      valve = octet;
-      state = WAITING_COMMAND_DATA;
-      break;
-
-    case WAITING_COMMAND_DATA:
-      if (valve >= VALID_PORTS_START && valve <= VALID_PORTS_END) {
-        // True => >0 => ouvrir la valve
-        // Sanity check on the pin value
-        digitalWrite(valve, octet ? HIGH : LOW);
-        Serial.print(valve);
-        Serial.print(octet);
-        Serial.print("\n");
-      }
-      state = WAITING_COMMAND;
-      break;
+    return;
   }
+
+  if (s_state != SS_IDLE) {
+    Serial.println("Received packet from master while not in idle state, dropping...");
+  }
+
+  // Read packet data from I2C channel.
+  uint8_t pkt_data_buf[BROUE_PACKET_SIZE];
+  for (int i = 0; i < BROUE_PACKET_SIZE; i++) {
+    pkt_data_buf[i] = Wire.read();
+  }
+  BrouePacket* pkt = reinterpret_cast<BrouePacket*>(&pkt_data_buf);
+
+  // Call handler for functionality.
+  if (packet_handlers[pkt->type]) {
+    Error status = packet_handlers[pkt->type](pkt);
+  }
+
+  // Change FSM state.  
+  switch (pkt->type) {
+    case BROUE_ACTUATE:
+      s_state = SS_ACTUATE_WAIT_READ;
+      break;
+
+    case BROUE_READ_SENSOR:
+      s_state = SS_READ_SENSOR_WAIT_READ;
+      break;
+
+    case BROUE_HELLO:
+      s_state = SS_HELLO_WAIT_READ;
+      break;
+
+    default:
+      Serial.print("Unexpected packet type : ");
+      Serial.println(BROUE_PACKET_TYPE_STR[pkt->type]);
+  }
+}
+
+void onReadRequest() {
+  // Check that we're in a read-wait state.
+  switch (s_state) {
+    case SS_ACTUATE_WAIT_READ:
+    case SS_READ_SENSOR_WAIT_READ:
+    case SS_HELLO_WAIT_READ:
+      break;
+
+    default:
+      Serial.println("Received read request while in invalid state");
+      return;
+  }
+
+  // Write prepared response packet to channel.
+  const uint8_t* pkt_data = reinterpret_cast<const uint8_t*>(&response_packet);
+  Wire.write(pkt_data, BROUE_PACKET_SIZE);
+
+  // Change FSM state.
+  s_state = SS_IDLE;
 }
---
+++ arduino/ardui_com/protocol.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <stdint.h>
+
+#define BROUE_PACKET_PAYLOAD_SIZE 16
+#define BROUE_PACKET_SIZE (BROUE_PACKET_PAYLOAD_SIZE + 1)
+
+enum BrouePacketType {
+  BROUE_ACK           = 0,
+  BROUE_ACTUATE,
+  BROUE_READ_SENSOR,
+  BROUE_HELLO,
+  BROUE_SENSOR_RESPONSE,
+  BROUE_TYPE_COUNT
+};
+
+const char* BROUE_PACKET_TYPE_STR[] = {
+  [BROUE_ACK] = "BROUE_ACK",
+  [BROUE_ACTUATE] = "BROUE_ACTUATE",
+  [BROUE_READ_SENSOR] = "BROUE_READ_SENSOR",
+  [BROUE_HELLO] = "BROUE_HELLO",
+  [BROUE_SENSOR_RESPONSE] = "BROUE_SENSOR_RESPONSE"
+};
+
+struct BroueAckPayload {
+  uint8_t ack_payload[BROUE_PACKET_PAYLOAD_SIZE];
+};
+
+struct BroueActuatePayload {
+  uint8_t actuator_id;
+  uint8_t actuator_command;
+
+  uint8_t pad[BROUE_PACKET_PAYLOAD_SIZE - 2];
+};
+
+struct BroueReadSensorPayload {
+  uint8_t sensor_id;
+
+  uint8_t pad[BROUE_PACKET_PAYLOAD_SIZE - 1];
+};
+
+struct BroueSensorValuePayload {
+  uint8_t sensor_id;
+
+  double sensor_value;
+
+  uint8_t pad[BROUE_PACKET_PAYLOAD_SIZE - 1 - sizeof(double)];
+};
+
+union BrouePacketPayload {
+  BroueAckPayload ack_payload;
+  BroueActuatePayload actuate_payload;
+  BroueReadSensorPayload read_sensor_payload;
+  BroueSensorValuePayload sensor_value_payload;
+};
+
+struct BrouePacket {
+  uint8_t type;
+  union BrouePacketPayload payload;
+};
+
+enum SlaveProtocolState {
+  SS_IDLE,
+  SS_ACTUATE_WAIT_READ,
+  SS_READ_SENSOR_WAIT_READ,
+  SS_HELLO_WAIT_READ
+};
