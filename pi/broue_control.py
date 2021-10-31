5612a8240c58ee7c881bbb3979a91d26d3bc251c
Fabien Portas <fabien.portas@grenoble-inp.org>
2021-10-31 15:55:05 -0400

    Clean up and add comments.

--- pi/broue_control.py
+++ pi/broue_control.py
@@ -1,13 +1,18 @@
-from typing import List
+# Need to install pigpio on Raspberry Pi
 import pigpio
 
 from struct import Struct
 from enum import IntEnum
+from typing import List
 
+# The I2C port number (1 by default on RPi)
 I2C_BUS = 1
+# Address of the slave Arduino on the bus (0x20 by default) 
 ARDUINO_I2C_ADDR = 0x20
+# Size of a packet, in bytes, mirroring protocol.h.
 BROUE_PACKET_SIZE = 17
 
+# Enum giving packet types, mirroring the values in C (protocol.hpp on Arduino side).
 class BrouePacketType(IntEnum):
     ACK             = 0
     ACTUATE         = 1
@@ -15,6 +20,9 @@ class BrouePacketType(IntEnum):
     HELLO           = 3
     SENSOR_RESPONSE = 4
 
+# Structure definitions for unpacking/packing C structs to send/receive as 
+# bytes on I2C.
+# See https://docs.python.org/3.8/library/struct.html for reference.
 broue_packet_struct = Struct("B16x")
 broue_ack_struct = Struct("B16x")
 broue_actuate_struct = Struct("BBB14x")
@@ -23,17 +31,16 @@ broue_hello_struct = Struct("B16x")
 broue_sensor_response = Struct("Bd8x")
 
 def str_to_bytes(data_as_str: str) -> List[int]:
+    """Util to turn a byte-array into a list for better display."""
     return [c for c in data_as_str]
 
 class Broue:
+    """
+        Main class to manage the link to the Arduino.
+    """
     def __init__(self, bus_num = I2C_BUS) -> None:
         self.pi = pigpio.pi()
         self.bus = self.pi.i2c_open(I2C_BUS, ARDUINO_I2C_ADDR, 0)
-        pass
-
-    def send_command(self, reg: int, val: int) -> None:
-        self.bus.write_byte(ARDUINO_I2C_ADDR, reg & 0xff)
-        self.bus.write_byte(ARDUINO_I2C_ADDR, val & 0xff)
 
     def actuate_valve(self, act_id: int, val: int) -> None:
         data = broue_actuate_struct.pack(BrouePacketType.ACTUATE.value, act_id, val)
@@ -83,21 +90,21 @@ class Broue:
         # Blocking read for ACK
         num_read, ack_data = self.pi.i2c_read_device(self.bus, BROUE_PACKET_SIZE)
         if num_read < 0:
-            print("Error on reading ACK from slave")
-            return
+            return False
         
+        # Check that it's actually an ACK.
         ack_fields = broue_ack_struct.unpack(ack_data)
         if BrouePacketType(ack_fields[0]) != BrouePacketType.ACK:
-            print("Received unexpected packet type from slave")
-            return
+            return False
+
+        return True
 
     def __del__(self):
         self.pi.stop()
         self.pi.i2c_close(self.bus)
-        pass
 
 # Exemple d'utilisation / tests
 br = Broue()
-br.actuate_valve(56, 1)
+br.actuate_valve(50, 1)
 br.read_sensor(42)
 br.send_hello()
